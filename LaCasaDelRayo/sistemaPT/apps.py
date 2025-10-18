e62e85983092260bbca8875d6e5c88ca1a34f773
bnjacduc <128086053+bnjacduc@users.noreply.github.com>
2025-10-17 23:10:37 -0300

    Delete LaCasaDelRayo directory

--- LaCasaDelRayo/sistemaPT/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SistemaptConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sistemaPT'
--- LaCasaDelRayo/sistemaPT/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('inicio', views.inicio, name='inicio'),  # ruta principal de la app
-    path('base/', views.base, name="base"),
-    path('', views.login_view, name='login'),
-]
